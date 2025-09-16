80b3c51879a68b69248e69f378599d54c3814b1b
Gayathri J Nair <gayathrij5003@gmail.com>
2025-09-16 22:52:58 +0530

    factorial and  fibonacci series  (iterative and recursive)

--- day2.py
+++ day2.py
@@ -1,22 +1,22 @@
 #factorial using iterative and recursive
-# def fact_iter(num):
-#     fact = 1
-#     for i in range(1, num+1):
-#         fact = fact * i
-#     return fact
-# def fact_recur(num):
-#     if num == 0 or num == 1:
-#         return 1
-#     else:
-#         return num * fact_recur(num - 1)
+def fact_iter(num):
+    fact = 1
+    for i in range(1, num+1):
+        fact = fact * i
+    return fact
+def fact_recur(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * fact_recur(num - 1)
 
-# num = int(input("enter a number: "))
-# inp = input("Enter the choice 1:itera/2:recur: ")
+num = int(input("enter a number: "))
+inp = input("Enter the choice 1:itera/2:recur: ")
 
-# if inp == "1":
-#     print("The result is : ",fact_iter(num))
-# elif inp == "2":
-#     print("The result is : ",fact_recur(num))
+if inp == "1":
+    print("The result is : ",fact_iter(num))
+elif inp == "2":
+    print("The result is : ",fact_recur(num))
 
 #fibanocci series
 
